92c578d31d63d939e0c484ff216a46f4e058bc9c
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-02-01 15:29:00 +0300

    discovery-server

--- productservice/src/main/java/etiya/productservice/ProductserviceApplication.java
+++ productservice/src/main/java/etiya/productservice/ProductserviceApplication.java
@@ -2,8 +2,12 @@ package etiya.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+// @EnableEurekaClient
+// Eski eureka client versiyonlarında gerekli!
 public class ProductserviceApplication {
 
     public static void main(String[] args) {
--- productservice/src/main/java/etiya/productservice/controllers/ProductsController.java
+++ productservice/src/main/java/etiya/productservice/controllers/ProductsController.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductsController {
     @GetMapping
     public boolean checkStock(@RequestParam int productId) {
+        System.out.println("İstek geldi.");
+        try{
+            Thread.sleep(3000);
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
         if(productId>5)
             return false;
         return true;
