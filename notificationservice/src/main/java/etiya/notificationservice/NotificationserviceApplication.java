f2badb072335c5caf48d5a5d330da03b1e484c4c
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-02-05 14:15:14 +0300

    .

--- notificationservice/src/main/java/etiya/notificationservice/NotificationserviceApplication.java
+++ notificationservice/src/main/java/etiya/notificationservice/NotificationserviceApplication.java
@@ -1,13 +1,21 @@
 package etiya.notificationservice;
 
+import com.etiya.orderservice.kafka.OrderCreatedEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class NotificationserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationserviceApplication.class, args);
+    }
+
+    @KafkaListener(topics = "Order", groupId = "OrderCreatedGroup")
+    public void handleOrderCreatedEvent(OrderCreatedEvent event)
+    {
+        System.out.println("Bir kafka eventi yakalandı:" + event.getCustomerName());
     }
 
 }
